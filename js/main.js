97c93b74c0f2bb35b519e4eb3426c07903e23063
Amir-mhd79 <Amirmohamad.akbarloo@gmail.com>
2023-08-25 12:48:53 +0330

    An exercise was done by Amirmohammad Akbarloo

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let button=document.querySelector("button.get")
+let fetchComments=()=>{
+    let html=""
+    let xml= new XMLHttpRequest()
+    xml.open("get","https://jsonplaceholder.typicode.com/comments")
+    xml.responseType="json"
+    xml.onload=()=>{
+        xml.response.forEach((elem)=>{
+            html+=`<div class="comment">
+                        <h2 class="postId">postId: ${elem.postId}</h2>
+                        <h2 class="id">ID: ${elem.id}</h2>
+                        <h2 class="name">Name: ${elem.name}</h2>
+                        <p class="email">Email: ${elem.email}</p>
+                        <p class="body">Comment: ${elem.body}</p>
+                   </div>`;
+        })
+        document.querySelector("div.wrapper").innerHTML=html
+    }
+    xml.send(null)
+}
+button.addEventListener("click",fetchComments)
